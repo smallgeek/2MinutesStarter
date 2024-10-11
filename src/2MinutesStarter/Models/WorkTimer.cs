c033ec827d3c29d92dbb51c66275927254128883
smallgeek <smallgeek@outlook.jp>
2024-10-11 16:27:25 +0900

    R3 で ViewModel を実装

---
+++ src/2MinutesStarter/Models/WorkTimer.cs
@@ -0,0 +1,37 @@
+﻿using R3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoMinutesStarter.Models
+{
+    public class WorkTimer : BindableBase
+    {
+        private DateTimeOffset startTime;
+
+        private TimeSpan elapsedTime;
+        public TimeSpan ElapsedTime
+        {
+            get => elapsedTime;
+            set => SetProperty(ref elapsedTime, value);
+        }
+
+        private IDisposable? interval;
+
+        public void Start()
+        {
+            startTime = DateTimeOffset.Now;
+
+            interval = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Subscribe(_ => ElapsedTime = DateTimeOffset.Now - startTime);
+        }
+
+        public void Stop()
+        {
+            interval?.Dispose();
+        }
+
+    }
+}
--- src/2MinutesStarter/ViewModels/MainWindowViewModel.cs
+++ src/2MinutesStarter/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using R3;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TwoMinutesStarter.Models;
 
 namespace TwoMinutesStarter.ViewModels
 {
-    public class MainWindowViewModel : BindableBase
+    public class MainWindowViewModel : BindableBase, IDisposable
     {
+        public BindableReactiveProperty<string> WorkName { get; }
+        public ReactiveCommand StartCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            WorkName = new BindableReactiveProperty<string>("");
+
+            StartCommand = WorkName
+                .Select(name => !string.IsNullOrWhiteSpace(name))
+                .ToReactiveCommand(_ =>
+                {
+                    // 画面遷移
+                    Debug.WriteLine("xxx");
+                }, initialCanExecute: false);
+        }
+
+        public void Dispose()
+        {
+            Disposable.Dispose(WorkName, StartCommand);
+        }
     }
 }
