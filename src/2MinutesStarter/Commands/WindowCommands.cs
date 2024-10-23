03adc5be5bd71b1803f4f0360b82334b1d05ab2c
smallgeek <smallgeek@outlook.jp>
2024-10-23 09:08:16 +0900

    作業中画面になったら最小化するように変更

---
+++ src/2MinutesStarter/Commands/WindowCommands.cs
@@ -0,0 +1,32 @@
+﻿using R3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TwoMinutesStarter.Commands
+{
+    /// <summary>
+    /// 画面に対する処理を行うコマンド群
+    /// </summary>
+    public class WindowCommands
+    {
+        /// <summary>
+        /// 最小化
+        /// </summary>
+        public ReactiveCommand Minimize { get; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public WindowCommands()
+        {
+            Minimize = new ReactiveCommand(static _ =>
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            });
+        }
+    }
+}
---
+++ src/2MinutesStarter/ViewModels/WorkingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoMinutesStarter.Commands;
+
+namespace TwoMinutesStarter.ViewModels
+{
+    /// <summary>
+    /// 作業中画面の ViewModel
+    /// </summary>
+    /// <param name="windowCommands"></param>
+    public class WorkingViewModel(WindowCommands windowCommands) : BindableBase, INavigationAware
+    {
+        public bool IsNavigationTarget(NavigationContext navigationContext) => true;
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            windowCommands.Minimize.Execute(R3.Unit.Default);
+        }
+    }
+}
